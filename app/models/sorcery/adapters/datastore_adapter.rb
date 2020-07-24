e46b4f751fb9f495e7f4d2d74e7b12f42fa21afc
Sumiki Ohsawa <sumikio@gmail.com>
2020-07-25 08:30:22 +0900

    find_by_credentials

--- app/models/sorcery/adapters/datastore_adapter.rb
+++ app/models/sorcery/adapters/datastore_adapter.rb
@@ -53,23 +53,11 @@ module Sorcery
         end
 
         def find_by_credentials(credentials)
-          relation = nil
-
           @klass.sorcery_config.username_attribute_names.each do |attribute|
-            if @klass.sorcery_config.downcase_username_before_authenticating
-              condition = @klass.arel_table[attribute].lower.eq(@klass.arel_table.lower(credentials[0]))
-            else
-              condition = @klass.arel_table[attribute].eq(credentials[0])
-            end
-
-            relation = if relation.nil?
-                         condition
-                       else
-                         relation.or(condition)
-                       end
+            @user = @klass.all(where: [attribute, '=', credentials[0]]).first
+            break if @user
           end
-
-          @klass.where(relation).first
+          @user
         end
 
         def find_by_token(token_attr_name, token)
